b2fbeca6f726441dc70a6b68fa7f760510f435f3
ViniciusCarvalhoDevy <vinicius.b.carvalho18@gmail.com>
2024-05-06 22:27:49 -0300

    feat: created interface globa for instances

---
+++ src/main/java/com/beautysalon/scheduling/util/interfaces/instancesGlobal.java
@@ -0,0 +1,7 @@
+package com.beautysalon.scheduling.util.interfaces;
+
+import org.modelmapper.ModelMapper;
+
+public interface instancesGlobal {
+    public ModelMapper mapper = new ModelMapper();
+}
